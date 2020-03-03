5acc47201d919b1cf0bcd5d34506568429ff1782
dkinguag <dkinguag@gmail.com>
2020-03-02 21:02:53 -0600

    added simple login page

--- gatsby-node.js
+++ gatsby-node.js
@@ -26,3 +26,4 @@ exports.createPages = ({ graphql, actions }) => {
     })
   })
 }
+
---
+++ src/pages/account.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Account = () => (
+  <div>
+    <p> TODO make this auth0 protected. </p>
+  </div>
+
+)
+
+export default Account
